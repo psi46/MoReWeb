7f777435642935933d5795b3197b1e44c4e52081
peller <peller>
2013-06-04 09:13:22 +0000

    Bugfix
    git-svn-id: https://svn.cern.ch/reps/moreweb@48 4525493e-7705-40b1-a816-d608a930855b

--- Analyse/TestResultClasses/CMSPixel/ModuleTestGroup/Module/RelativeGainWidth/TestResult.py
+++ Analyse/TestResultClasses/CMSPixel/ModuleTestGroup/Module/RelativeGainWidth/TestResult.py
@@ -16,7 +16,7 @@ class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
 		self.ResultData['HiddenData']['LimitC'] = self.TestResultEnvironmentObject.GradingParameters['gainC']
 		self.ParentObject.ResultData['SubTestResults']['Noise'].SpecialPopulateData(self, {
 				'Key':'RelativeGainWidth',
-				'DataKey':'SCurveWidths', # which sub test result to take the data from
+				'DataKey':'PHCalibrationGain', # which sub test result to take the data from
 				'DataParameterKey':'mu', # which part of key value dict pairs
 				'CalcFunction':lambda Value, KeyValueDictPairs: float(KeyValueDictPairs['sigma']['Value']) / float(KeyValueDictPairs['mu']['Value']) if float(KeyValueDictPairs['mu']['Value'])>0 else 0,
 				'YLimitB':self.TestResultEnvironmentObject.GradingParameters['gainB'],# limit for grading
