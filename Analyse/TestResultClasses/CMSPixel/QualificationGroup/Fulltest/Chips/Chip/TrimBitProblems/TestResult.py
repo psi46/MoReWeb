9d973d7890c2fb4d0cbcd9e03e4a412c09caebde
veloxid <bachmair@phys.ethz.ch>
2014-06-25 17:35:40 +0200

    typo in TrimBitProblems

--- Analyse/TestResultClasses/CMSPixel/QualificationGroup/Fulltest/Chips/Chip/TrimBitProblems/TestResult.py
+++ Analyse/TestResultClasses/CMSPixel/QualificationGroup/Fulltest/Chips/Chip/TrimBitProblems/TestResult.py
@@ -81,6 +81,6 @@ class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
             if TrimBitDifference <= gradingCriteria :
                 self.DeadTrimbitsList.add((self.chipNo, column, row))
                 retVal += 2 ** (4 - (k - 1))
-                if self.verbosity:
+                if self.verbose:
                     print 'Dead TrimBit: added %2d,%2d %d' % (column, row, k), trimBitK, trimBit0, TrimBitDifference, gradingCriteria, (TrimBitDifference <= gradingCriteria), retVal
         return retVal
