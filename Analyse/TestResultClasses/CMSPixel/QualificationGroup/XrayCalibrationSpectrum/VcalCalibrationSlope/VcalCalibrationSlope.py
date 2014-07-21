0ae015a6def49fc47aa5be5e7436c89b1066e868
Felix Bachmair <bachmair@phys.ethz.ch>
2014-07-21 16:10:54 +0200

    updated verbose, new Title, removed unnecassary function, cleanup

--- Analyse/TestResultClasses/CMSPixel/QualificationGroup/XrayCalibrationSpectrum/VcalCalibrationSlope/VcalCalibrationSlope.py
+++ Analyse/TestResultClasses/CMSPixel/QualificationGroup/XrayCalibrationSpectrum/VcalCalibrationSlope/VcalCalibrationSlope.py
@@ -1,56 +1,42 @@
 import AbstractClasses
 import ROOT
 
+
 class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
     def CustomInit(self):
         self.Name = "CMSPixel_QualificationGroup_XrayCalibrationSpectrum_VcalCalibrationSlope_TestResult"
         self.NameSingle = "VcalCalibrationSlope"
-        self.Title = "Slope of Vcal Calibration"
-        self.verbose = False
+        self.method = self.Attributes['Method']
+        self.Title = "Slope of {Method} Vcal Calibration".format(Method=self.method)
         if self.verbose:
             tag = self.Name + ": Custom Init"
             print "".ljust(len(tag), '=')
             print tag
         self.ResultData["SubTestResultDictList"] = []
         self.check_Test_Software()
-        #self.ModuleVersion, self.nRocs, self.halfModule
-        #self.nRocs =
-        # print   self.ParentObject.ResultData['SubTestResults']
-
-                # , self.ParentObject.ResultData['SubTestResultDictList'][i]
-        # ROCtype, nRocs, halfModule = self.ReadModuleVersion()
-        # print ROCtype,nRocs,halfModule
-        # self.nRocs = self.ParentObject.Attributes['NumberOfChips']
-        # Get module information
-
-
         self.Attributes['TestedObjectType'] = 'VcalCalibrationSlope'
 
-    def get_n_rocs(self):
-        self.nRocs = 0
-        for i in self.ParentObject.ResultData['SubTestResultDictList']:
-            if i['Key'].startswith('FluorescenceSpectrumModule'):
-                self.nRocs = i['TestResultObject'].ResultData['HiddenData']['nRocs']
-                break
-    def median(self,l):
+    def median(self, l):
         s = sorted(l)
         i = len(s)
-        if not i%2:
-            return (s[(i/2)-1]+s[i/2])/2.0
-        return s[i/2]
+        if not i % 2:
+            return (s[(i / 2) - 1] + s[i / 2]) / 2.0
+        return s[i / 2]
 
     def PopulateResultData(self):
+        if self.method == 'SCurve':
+            return
         if self.verbose:
             tag = self.Name + ": Populate"
             print "".ljust(len(tag), '=')
             print tag
-        self.get_n_rocs()
+        self.nRocs = self.ParentObject.nRocs
         slopes = []
         error_slopes = []
         for roc in range(self.nRocs):
             vcal_calibration_module = self.ParentObject.ResultData['SubTestResults']['VcalCalibrationModule']
-            roc_results = vcal_calibration_module.ResultData['SubTestResults'][
-                'VcalCalibrationROC%i' % (roc)].ResultData
+            key = "VcalCalibration_{Method}_ROC{ROC}".format(Method=self.Attributes['Method'], ROC=roc)
+            roc_results = vcal_calibration_module.ResultData['SubTestResults'][key].ResultData
             roc_slope = roc_results['KeyValueDictPairs']['Slope']['Value']
             roc_slope_error = roc_results['KeyValueDictPairs']['Slope']['Sigma']
             error_slopes.append(roc_slope_error)
@@ -58,22 +44,22 @@ class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
                 print roc, roc_slope, roc_slope_error
             slopes.append(roc_slope)
 
-        self.SpecialPopulateData(self,slopes,error_slopes,{'Key':'Slope',
-                'MarkerColor':ROOT.kPink,
-                'LineColor':ROOT.kPink,
-                'MarkerStyle':21,
-                'YaxisTitle':'Slope [e- / Vcal]',})
+        self.SpecialPopulateData(self, slopes, error_slopes, {'Key': 'Slope',
+                                                              'MarkerColor': ROOT.kPink,
+                                                              'LineColor': ROOT.kPink,
+                                                              'MarkerStyle': 21,
+                                                              'YaxisTitle': 'Slope [e- / Vcal]', })
 
-    def SpecialPopulateData(self,TestResultObject, array, error_array, Parameters):
+    def SpecialPopulateData(self, TestResultObject, array, error_array, Parameters):
         TestResultObject.ResultData['Plot']['ROOTObject'] = ROOT.TH1D(self.GetUniqueID(), '', TestResultObject.nRocs, 0,
-                                                          TestResultObject.nRocs)
+                                                                      TestResultObject.nRocs)
         with_errors = (len(array) == len(error_array))
         for i in range(len(array)):
             TestResultObject.ResultData['Plot']['ROOTObject'].SetBinContent(i + 1, array[i])
             if with_errors:
-                TestResultObject.ResultData['Plot']['ROOTObject'].SetBinError(i+1, error_array[i])
+                TestResultObject.ResultData['Plot']['ROOTObject'].SetBinError(i + 1, error_array[i])
         min_array = min(array)
-        max_array= max(array)
+        max_array = max(array)
         median_array = self.median(array)
         average_array = reduce(lambda x, y: x + y, array) / len(array)
         ymin = min_array
@@ -107,47 +93,37 @@ class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
         lineB.SetLineColor(ROOT.kRed)
 
         TestResultObject.ResultData['KeyValueDictPairs'] = {
-            'avrg%s'%Parameters['Key'] : {
-                "Value" : average_array,
-                "Sigma" : 0,
-                "Label" : 'avrg %s'%Parameters['Key'],
-                "Unit" : "",
+            'avrg%s' % Parameters['Key']: {
+                "Value": average_array,
+                "Sigma": 0,
+                "Label": 'avrg %s' % Parameters['Key'],
+                "Unit": "",
             },
-            'min%s'%Parameters['Key'] : {
-                "Value" : min_array,
-                "Sigma" : 0,
-                "Label" : 'min %s'%Parameters['Key'],
-                "Unit" : "",
+            'min%s' % Parameters['Key']: {
+                "Value": min_array,
+                "Sigma": 0,
+                "Label": 'min %s' % Parameters['Key'],
+                "Unit": "",
             },
-            'max%s'%Parameters['Key'] : {
-                "Value" : max_array,
-                "Sigma" : 0,
-                "Label" : 'max %s'%Parameters['Key'],
-                "Unit" : "",
+            'max%s' % Parameters['Key']: {
+                "Value": max_array,
+                "Sigma": 0,
+                "Label": 'max %s' % Parameters['Key'],
+                "Unit": "",
             },
-            'median%s'%Parameters['Key'] : {
-                "Value" : median_array,
-                "Sigma" : 0,
-                "Label" : 'median %s'%Parameters['Key'],
-                "Unit" : "",
+            'median%s' % Parameters['Key']: {
+                "Value": median_array,
+                "Sigma": 0,
+                "Label": 'median %s' % Parameters['Key'],
+                "Unit": "",
             }
         }
-        TestResultObject.ResultData['KeyList'].append( 'avrg%s'%Parameters['Key'])
-        TestResultObject.ResultData['KeyList'].append('median%s'%Parameters['Key'])
-        TestResultObject.ResultData['KeyList'].append('min%s'%Parameters['Key'])
-        TestResultObject.ResultData['KeyList'].append('max%s'%Parameters['Key'])
+        TestResultObject.ResultData['KeyList'].append('avrg%s' % Parameters['Key'])
+        TestResultObject.ResultData['KeyList'].append('median%s' % Parameters['Key'])
+        TestResultObject.ResultData['KeyList'].append('min%s' % Parameters['Key'])
+        TestResultObject.ResultData['KeyList'].append('max%s' % Parameters['Key'])
 
         TestResultObject.Canvas.SaveAs(TestResultObject.GetPlotFileName())
         TestResultObject.ResultData['Plot']['Enabled'] = 1
         TestResultObject.ResultData['Plot']['Caption'] = Parameters['Key']
         TestResultObject.ResultData['Plot']['ImageFile'] = TestResultObject.GetPlotFileName()
-        #self.ResultData['Table'] = {
-        #    'HEADER':[
-        #        [
-        #            'ROC', 'Vcal'
-        #        ]
-        #    ],
-        #    'BODY':[],
-        #    'FOOTER':[],
-        #}
-
