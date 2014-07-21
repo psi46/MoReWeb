f6136e0931b7eebead3e5e24a3665b1b84a5ee7f
Felix Bachmair <bachmair@phys.ethz.ch>
2014-07-21 14:30:16 +0200

    chnged title, removed some verbose level, changed keys of subtests

--- Analyse/TestResultClasses/CMSPixel/QualificationGroup/XrayCalibrationSpectrum/VcalCalibrationOffset/VcalCalibrationOffset.py
+++ Analyse/TestResultClasses/CMSPixel/QualificationGroup/XrayCalibrationSpectrum/VcalCalibrationOffset/VcalCalibrationOffset.py
@@ -3,10 +3,10 @@ import ROOT
 
 class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
     def CustomInit(self):
+        self.method = self.Attributes['Method']
         self.Name = "CMSPixel_QualificationGroup_XrayCalibrationSpectrum_VcalCalibrationOffset_TestResult"
         self.NameSingle = "VcalCalibrationOffset"
-        self.Title = "Offset of Vcal Calibration"
-        self.verbose = False
+        self.Title = "Offset of {Method} Vcal Calibration".format(Method = self.method)
         if self.verbose:
             tag = self.Name + ": Custom Init"
             print "".ljust(len(tag), '=')
@@ -24,25 +24,25 @@ class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
 
 
     def PopulateResultData(self):
+        if self.method == 'SCurve':
+            return
         if self.verbose:
             tag = self.Name + ": Populate"
             print "".ljust(len(tag), '=')
             print tag
-        self.get_n_rocs()
+        self.nRocs = self.ParentObject.nRocs
         offsets = []
         error_offsets = []
         for roc in range(self.nRocs):
             vcal_calibration_module = self.ParentObject.ResultData['SubTestResults']['VcalCalibrationModule']
-            roc_results = vcal_calibration_module.ResultData['SubTestResults'][
-                'VcalCalibrationROC%i' % (roc)].ResultData
+            key = "VcalCalibration_{Method}_ROC{ROC}".format(Method=self.Attributes['Method'], ROC=roc)
+            roc_results = vcal_calibration_module.ResultData['SubTestResults'][key].ResultData
             roc_offset = roc_results['KeyValueDictPairs']['Offset']['Value']
             roc_offset_error = roc_results['KeyValueDictPairs']['Offset']['Sigma']
             if self.verbose:
                 print roc, roc_offset, roc_offset_error
             offsets.append(roc_offset)
             error_offsets.append(roc_offset_error)
-        for i in  self.ParentObject.ResultData['SubTestResultDictList']:
-            print i
         vcal_calibration = self.ParentObject.ResultData['SubTestResults']['VcalCalibrationSlope']
         vcal_calibration.SpecialPopulateData(self,offsets,error_offsets,{'Key':'Offset',
                 'MarkerColor':ROOT.kPink,
