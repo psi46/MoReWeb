03b4b1caa76a31d4227a2cc76a9a968805af6b7c
Felix Bachmair <bachmair@phys.ethz.ch>
2014-07-24 16:21:36 +0200

    added Chi2 per NDF + some cleanup

--- Analyse/TestResultClasses/CMSPixel/QualificationGroup/XrayCalibrationSpectrum/VcalCalibrationModule/VcalCalibrationROC/VcalCalibrationROC.py
+++ Analyse/TestResultClasses/CMSPixel/QualificationGroup/XrayCalibrationSpectrum/VcalCalibrationModule/VcalCalibrationROC/VcalCalibrationROC.py
@@ -5,7 +5,8 @@ import array
 
 class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
     def CustomInit(self):
-        self.Name = "CMSPixel_QualificationGroup_XrayCalibrationSpectrum_VcalCalibrationModule_VcalCalibrationROC_TestResult"
+        self.Name = "CMSPixel_QualificationGroup_XrayCalibrationSpectrum"
+        self.Name += "_VcalCalibrationModule_VcalCalibrationROC_TestResult"
         self.NameSingle = "VcalCalibrationROC"
         self.Title = "Vcal Calibration ROC %i" % (self.Attributes['ChipNo'])
         self.ChipNo = self.Attributes['ChipNo']
@@ -29,98 +30,100 @@ class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
             print "".ljust(len(tag), '=')
             print tag
 
-        fitOption = "Q"
+        fit_option = "Q"
         if not self.verbose:
-            fitOption += 'Q'
-        PeakCenters = array.array('d', [])
-        PeakErrors = array.array('d', [])
-        NumElectrons = array.array('d', [])
+            fit_option += 'Q'
+        peak_centers = array.array('d', [])
+        peak_errors = array.array('d', [])
+        n_electrons = array.array('d', [])
         top_parent = self.ParentObject.ParentObject
         for test in top_parent.ResultData['SubTestResults']:
             if not "FluorescenceSpectrumModule" in test:
                 continue
             module_results = top_parent.ResultData['SubTestResults'][test].ResultData['SubTestResults']
             roc_results = module_results['FluorescenceSpectrum_C%i' % (self.Attributes["ChipNo"])]
-            keyValuePairs = roc_results.ResultData['KeyValueDictPairs']
+            key_value_pairs = roc_results.ResultData['KeyValueDictPairs']
             if self.verbose:
-                print test, keyValuePairs['Center']['Value'], keyValuePairs['TargetNElectrons']['Value']
-            PeakCenters.append(keyValuePairs['Center']['Value'])
-            PeakErrors.append(keyValuePairs['Center']['Sigma'])
-            NumElectrons.append(keyValuePairs['TargetNElectrons']['Value'])
-        pointPairs = zip(PeakCenters, NumElectrons, PeakErrors)
-        sortedPoints = sorted(pointPairs, key=lambda point: point[1])
-        print sortedPoints
-        prevPoint = sortedPoints[0][0]
+                print test, key_value_pairs['Center']['Value'], key_value_pairs['TargetNElectrons']['Value']
+            peak_centers.append(key_value_pairs['Center']['Value'])
+            peak_errors.append(key_value_pairs['Center']['Sigma'])
+            n_electrons.append(key_value_pairs['TargetNElectrons']['Value'])
+        point_pairs = zip(peak_centers, n_electrons, peak_errors)
+        sorted_points = sorted(point_pairs, key=lambda point: point[1])
+        print sorted_points
+        prev_point = sorted_points[0][0]
         num = 0
-        for e in sortedPoints:
-            if e[0] < prevPoint:
+        for e in sorted_points:
+            if e[0] < prev_point:
                 if self.verbose:
                     print "Error: Lower VCal for higher energy...possible fit error. Point in question: ", e
-                sortedPoints.pop(num)
-            num = num + 1
-            prevPoint = e[0]
-        newSortedPoints = sorted(sortedPoints, key=lambda point: point[0])
-        sortedPeakCenters = array.array('d', [])
-        sortedNumElectrons = array.array('d', [])
-        sortedPeakErrors = array.array('d', [])
-        sortedElectronError = array.array('d', [])
-        for e in newSortedPoints:
-            sortedPeakCenters.append(e[0])
-            sortedNumElectrons.append(e[1])
-            sortedPeakErrors.append(e[2])
-            sortedElectronError.append(0.0)
-        self.ResultData['Plot']['ROOTObject'] = ROOT.TGraphErrors(len(sortedPeakCenters), sortedPeakCenters,
-                                                                  sortedNumElectrons, sortedPeakErrors,
-                                                                  sortedElectronError)
-        self.ResultData['Plot']['ROOTObject'].SetTitle(
-            "Center of Pulse Height (Vcal units) vs number of electrons;Center of Pulse Height[Vcal];Number of Electrons")
+                sorted_points.pop(num)
+            num += 1
+            prev_point = e[0]
+        new_sorted_points = sorted(sorted_points, key=lambda point: point[0])
+        sorted_peak_centers = array.array('d', [])
+        sorted_n_electrons = array.array('d', [])
+        sorted_peak_errors = array.array('d', [])
+        sorted_electron_error = array.array('d', [])
+        for e in new_sorted_points:
+            sorted_peak_centers.append(e[0])
+            sorted_n_electrons.append(e[1])
+            sorted_peak_errors.append(e[2])
+            sorted_electron_error.append(0.0)
+        self.ResultData['Plot']['ROOTObject'] = ROOT.TGraphErrors(len(sorted_peak_centers), sorted_peak_centers,
+                                                                  sorted_n_electrons, sorted_peak_errors,
+                                                                  sorted_electron_error)
+        title = "Center of Pulse Height (Vcal units) vs number of electrons;"
+        title += "Center of Pulse Height[Vcal];"
+        title += "Number of Electrons"
+        self.ResultData['Plot']['ROOTObject'].SetTitle(title)
         self.ResultData['Plot']['ROOTObject'].SetMarkerColor(4)
         self.ResultData['Plot']['ROOTObject'].GetYaxis().SetTitleOffset(1.6)
         # self.ResultData['Plot']['ROOTObject'].SetMarkerStyle(21)
 
-        #Fitting of Slope
-        self.ResultData['Plot']['ROOTObject'].Fit("pol1", fitOption, "SAME", sortedPeakCenters[0],
-                                                  sortedPeakCenters[len(sortedPeakCenters) - 1])
-        chi2Total = self.ResultData['Plot']['ROOTObject'].GetFunction("pol1").GetChisquare()
+        # Fitting of Slope
+        self.ResultData['Plot']['ROOTObject'].Fit("pol1", fit_option, "SAME", sorted_peak_centers[0],
+                                                  sorted_peak_centers[len(sorted_peak_centers) - 1])
+        chi2_total = self.ResultData['Plot']['ROOTObject'].GetFunction("pol1").GetChisquare()
         ndf_total = self.ResultData['Plot']['ROOTObject'].GetFunction("pol1").GetNDF()
         if ndf_total > 0:
-            chi2Total /= ndf_total
-        self.ResultData['Plot']['ROOTObject'].Fit("pol1", fitOption, "SAME", sortedPeakCenters[1],
-                                                  sortedPeakCenters[len(sortedPeakCenters) - 1])
-        chi2Right = self.ResultData['Plot']['ROOTObject'].GetFunction("pol1").GetChisquare()
+            chi2_total /= ndf_total
+        self.ResultData['Plot']['ROOTObject'].Fit("pol1", fit_option, "SAME", sorted_peak_centers[1],
+                                                  sorted_peak_centers[len(sorted_peak_centers) - 1])
+        chi2_right = self.ResultData['Plot']['ROOTObject'].GetFunction("pol1").GetChisquare()
         ndf_right = self.ResultData['Plot']['ROOTObject'].GetFunction("pol1").GetNDF()
         if ndf_right > 0:
-            chi2Right /= ndf_right
-        self.ResultData['Plot']['ROOTObject'].Fit("pol1", fitOption, "SAME", sortedPeakCenters[0],
-                                                  sortedPeakCenters[len(sortedPeakCenters) - 2])
-        chi2Left = self.ResultData['Plot']['ROOTObject'].GetFunction("pol1").GetChisquare()
+            chi2_right /= ndf_right
+        self.ResultData['Plot']['ROOTObject'].Fit("pol1", fit_option, "SAME", sorted_peak_centers[0],
+                                                  sorted_peak_centers[len(sorted_peak_centers) - 2])
+        chi2_left = self.ResultData['Plot']['ROOTObject'].GetFunction("pol1").GetChisquare()
         ndf_left = self.ResultData['Plot']['ROOTObject'].GetFunction("pol1").GetNDF()
         if ndf_left > 0:
-            chi2Left /= ndf_left
+            chi2_left /= ndf_left
         if self.verbose:
             print 'Comparing Chi^2 for different fit ranges:'
-            print '\tchi2Total ', chi2Total, ' @ NDF ', ndf_total
-            print '\tchi2Left  ', chi2Left, ' @ NDF ', ndf_left
-            print '\tchi2Right ', chi2Right, ' @ NDF ', ndf_right
-        if (((chi2Right < chi2Total) or (chi2Left < chi2Total)) and ndf_total > 1):
-            if chi2Right < chi2Left:
-                self.ResultData['Plot']['ROOTObject'].Fit("pol1", fitOption, "SAME", sortedPeakCenters[1],
-                                                          sortedPeakCenters[len(sortedPeakCenters) - 1])
+            print '\tchi2Total ', chi2_total, ' @ NDF ', ndf_total
+            print '\tchi2Left  ', chi2_left, ' @ NDF ', ndf_left
+            print '\tchi2Right ', chi2_right, ' @ NDF ', ndf_right
+        if ((chi2_right < chi2_total) or (chi2_left < chi2_total)) and ndf_total > 1:
+            if chi2_right < chi2_left:
+                self.ResultData['Plot']['ROOTObject'].Fit("pol1", fit_option, "SAME", sorted_peak_centers[1],
+                                                          sorted_peak_centers[len(sorted_peak_centers) - 1])
                 if self.verbose:
-                    print "Excluding Leftmost Point because chi2Total=", chi2Total, " and chi2Right=", chi2Right
+                    print "Excluding Leftmost Point because chi2Total=", chi2_total, " and chi2Right=", chi2_right
             else:
-                self.ResultData['Plot']['ROOTObject'].Fit("pol1", fitOption, "SAME", sortedPeakCenters[0],
-                                                          sortedPeakCenters[len(sortedPeakCenters) - 2])
+                self.ResultData['Plot']['ROOTObject'].Fit("pol1", fit_option, "SAME", sorted_peak_centers[0],
+                                                          sorted_peak_centers[len(sorted_peak_centers) - 2])
                 if self.verbose:
-                    print "Excluding Rightmost Point because chi2Total=", chi2Total, " and chi2Left=", chi2Left
+                    print "Excluding Rightmost Point because chi2Total=", chi2_total, " and chi2Left=", chi2_left
         else:
-            self.ResultData['Plot']['ROOTObject'].Fit("pol1", fitOption, "SAME", sortedPeakCenters[0],
-                                                      sortedPeakCenters[len(sortedPeakCenters) - 1])
+            self.ResultData['Plot']['ROOTObject'].Fit("pol1", fit_option, "SAME", sorted_peak_centers[0],
+                                                      sorted_peak_centers[len(sorted_peak_centers) - 1])
 
         fit = self.ResultData['Plot']['ROOTObject'].GetFunction("pol1")
-        name = 'linFit_{Method}_C{ChipNo}'.format(Method = self.Method,ChipNo = self.ChipNo)
+        name = 'linFit_{Method}_C{ChipNo}'.format(Method=self.Method, ChipNo=self.ChipNo)
         self.ResultData['Plot']['ROOTObject'].GetListOfFunctions().Add(fit.Clone(name))
-        self.ResultData['Plot']['ROOTObject'].GetFunction(name).SetRange(0,255)
+        self.ResultData['Plot']['ROOTObject'].GetFunction(name).SetRange(0, 255)
         self.ResultData['Plot']['ROOTObject'].GetFunction(name).SetLineStyle(2)
 
         self.ResultData['KeyValueDictPairs'] = {
@@ -135,6 +138,12 @@ class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
                 'Label': 'Offset',
                 'Unit': 'nElectrons',
                 'Sigma': round(fit.GetParError(0), 3),
+            },
+            'chi2': {
+                'Value': round(fit.GetParameter(0), 3),
+                'Label': 'Chi2',
+                'Unit': 'per NDF',
+                'Sigma': round(fit.GetChisquare()/fit.GetNDF(), 3),
             },
 
         }
