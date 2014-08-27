b62c39d05f3ba32a779326fcf40de753dcb6eee5
Felix Bachmair <bachmair@phys.ethz.ch>
2014-08-27 15:15:31 +0200

    updating main temp plot

--- Analyse/TestResultClasses/CMSPixel/QualificationGroup/Temperature/Temperature.py
+++ Analyse/TestResultClasses/CMSPixel/QualificationGroup/Temperature/Temperature.py
@@ -6,79 +6,79 @@ import ConfigParser
 import AbstractClasses.Helper.helper as Helper
 import math
 import array
+from AbstractClasses.GeneralTestResult import GeneralTestResult
 
-class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
+
+class TestResult(GeneralTestResult):
     def CustomInit(self):
-        self.Name='CMSPixel_QualificationGroup_TemperatureCycle_TestResult'
-        self.NameSingle='TemperatureCycle'
+        self.Name = 'CMSPixel_QualificationGroup_TemperatureCycle_TestResult'
+        self.NameSingle = 'TemperatureCycle'
 
         self.Title = str(self.Attributes['ModuleID']) + ' ' + self.Attributes['StorageKey']
         self.Attributes['TestedObjectType'] = 'CMSPixel_Module'
 
-
     def OpenFileHandle(self):
         self.FileHandle = ConfigParser.ConfigParser()
-        fileName = self.RawTestSessionDataPath+'/elComandante.ini'
+        fileName = self.RawTestSessionDataPath + '/elComandante.ini'
         if not os.path.isfile(fileName):
-            fileName = self.RawTestSessionDataPath+'/Tests.ini'
-#        print 'open ConfigFile "%s"'%fileName
+            fileName = self.RawTestSessionDataPath + '/Tests.ini'
+        # print 'open ConfigFile "%s"'%fileName
         self.FileHandle.read(fileName)
 
-
-    def analyseTemp(self,fileName):
-        print 'analyse Temp for "%s"'%fileName
-        duration = 0
-        temp = 0
-        tempError = 0
-        tempMin = 0
-        tempMax = 0
-        timeMin = 0
-        timeMax = 0
+    def analyseTemp(self, fileName):
+        print 'analyse Temp for "%s"' % fileName
+        # duration = 0
+        # temp = 0
+        # tempError = 0
+        # tempMin = 0
+        # tempMax = 0
+        # timeMin = 0
+        # timeMax = 0
         name = fileName.split('/')[-1].split('.')[0]
         name.strip()
-        varlist = 'time:temp'
+        # varlist = 'time:temp'
         if Helper.fileExists(fileName):
-            file = open(fileName)
-            lines = file.readlines()
+            this_file = open(fileName)
+            lines = this_file.readlines()
             lines = [i for i in lines if not i.startswith('#')]
-            tuples = [i.strip().split('\t') for i  in lines]
+            tuples = [i.strip().split('\t') for i in lines]
             times = [int(i[0]) for i in tuples]
             temps = [float(i[1]) for i in tuples]
-            if len(temps)>0:
-                temp = sum(temps)/len(temps)
-                temp2 = sum([i*i for i in temps])/len(temps)
+            if len(temps) > 0:
+                temp = sum(temps) / len(temps)
+                temp2 = sum([i * i for i in temps]) / len(temps)
             else:
                 temp = 0
                 temp2 = 0
-                tempMin = 0
-                tempMax = 0
+                # tempMin = 0
+                # tempMax = 0
                 timeMin = 0
                 timeMax = 0
-#
-#             # get RMS Temp
-            tempError = math.sqrt(temp2-temp*temp)
-#             ROOT.TMath.RMS(tuple.GetSelectedRows(),tuple.GetV1())
-#
-            if len(temps)>0:
-#             # get Min Temp
+            #
+            # # get RMS Temp
+            # tempError = math.sqrt(temp2 - temp * temp)
+            # ROOT.TMath.RMS(tuple.GetSelectedRows(),tuple.GetV1())
+            #
+            if len(temps) > 0:
+                #             # get Min Temp
                 tempMin = min(temps)
-    #             #get Max Temp
+                #             #get Max Temp
                 tempMax = max(temps)
                 #calculate time difference
                 timeMin = min(times)
                 timeMax = max(times)
-#
-            duration = timeMax - timeMin
-            temp_List =   array.array('d',temps)
+            #
+            # duration = timeMax - timeMin
+            temp_List = array.array('d', temps)
             time_List = array.array('d', times)
             if not self.ResultData['Plot'].has_key('ROOTObjects'):
-                self.ResultData['Plot']['ROOTObjects']={}
-            name = '%02d_%s'%(len(self.ResultData['Plot']['ROOTObjects']),name)
+                self.ResultData['Plot']['ROOTObjects'] = {}
+            # name = '%02d_%s' % (len(self.ResultData['Plot']['ROOTObjects']), name)
             if len(temps):
-                graph = ROOT.TGraph(len(temp_List), time_List,temp_List)
+                graph = ROOT.TGraph(len(temp_List), time_List, temp_List)
                 self.ResultData['Plot']['ROOTObject'] = ROOT.TMultiGraph()
             else:
-                graph =  ROOT.TGraph()
+                graph = ROOT.TGraph()
 
             canvas = self.TestResultEnvironmentObject.Canvas
             self.CanvasSize(canvas)
@@ -103,27 +103,25 @@ class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
             canvas.Clear()
             if self.ResultData['Plot']['ROOTObject']:
                 if graph:
-                    self.ResultData['Plot']['ROOTObject'].Add(graph, "P")
+                    self.ResultData['Plot']['ROOTObject'].Add(graph, "L")
                 self.ResultData['Plot']['ROOTObject'].Draw("a")
                 self.ResultData['Plot']['ROOTObject'].SetTitle(';Time; Temp [#circ C]')
                 self.ResultData['Plot']['ROOTObject'].GetXaxis().SetTimeDisplay(1)
-                self.ResultData['Plot']['ROOTObject'].GetYaxis().SetDecimals();
-                self.ResultData['Plot']['ROOTObject'].GetYaxis().SetTitleOffset(1.5);
-                self.ResultData['Plot']['ROOTObject'].GetYaxis().CenterTitle();
+                self.ResultData['Plot']['ROOTObject'].GetYaxis().SetDecimals()
+                self.ResultData['Plot']['ROOTObject'].GetYaxis().SetTitleOffset(1.5)
+                self.ResultData['Plot']['ROOTObject'].GetYaxis().CenterTitle()
             self.Canvas = canvas
 
     def PopulateResultData(self):
         fileHandlePath = self.RawTestSessionDataPath
         print fileHandlePath
-        self.analyseTemp(fileHandlePath+'/temperature.log')
+        self.analyseTemp(fileHandlePath + '/temperature.log')
         if self.verbose: raw_input('Press enter')
         if self.SavePlotFile:
             self.Canvas.SaveAs(self.GetPlotFileName())
         self.ResultData['Plot']['Enabled'] = 1
         self.ResultData['Plot']['Caption'] = 'Temperature'
         self.ResultData['Plot']['ImageFile'] = self.GetPlotFileName()
-
-
 
     def CustomWriteToDatabase(self, ParentID):
         pass
