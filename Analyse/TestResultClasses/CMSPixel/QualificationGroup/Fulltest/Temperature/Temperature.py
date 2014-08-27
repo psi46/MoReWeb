68ad061902e7d04e9dba0f990c0a1e63d8625e43
Felix Bachmair <bachmair@phys.ethz.ch>
2014-08-27 15:15:23 +0200

    updating main temp plot

--- Analyse/TestResultClasses/CMSPixel/QualificationGroup/Fulltest/Temperature/Temperature.py
+++ Analyse/TestResultClasses/CMSPixel/QualificationGroup/Fulltest/Temperature/Temperature.py
@@ -1,21 +1,24 @@
 # -*- coding: utf-8 -*-
-import ROOT
-import AbstractClasses
 import datetime
-import AbstractClasses.Helper.helper as Helper
 import array
 import math
-class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
+
+import ROOT
+
+import AbstractClasses
+import AbstractClasses.Helper.helper as Helper
+from AbstractClasses.GeneralTestResult import GeneralTestResult
+
+
+class TestResult(GeneralTestResult):
     def CustomInit(self):
-        self.Name='CMSPixel_QualificationGroup_Temperature_TestResult'
-        self.NameSingle='Temperature'
+        self.Name = 'CMSPixel_QualificationGroup_Temperature_TestResult'
+        self.NameSingle = 'Temperature'
         self.Attributes['TestedObjectType'] = 'CMSPixel_Module'
         self.Title = 'Temperature'
 
-
-
-    def analyseTemp(self,fileName):
-        print 'analyse Temp for "%s"'%fileName
+    def analyseTemp(self, fileName):
+        print 'analyse Temp for "%s"' % fileName
         duration = 0
         temp = 0
         tempError = 0
@@ -25,19 +28,19 @@ class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
         timeMax = 0
         name = fileName.split('/')[-1].split('.')[0]
         name.strip()
-        varlist = 'time:temp'
+        # varlist = 'time:temp'
         if not self.ResultData['Plot'].has_key('ObjectCanvas'):
-                self.ResultData['Plot']['ObjectCanvas'] = {}
+            self.ResultData['Plot']['ObjectCanvas'] = {}
         if Helper.fileExists(fileName):
-            file = open(fileName)
-            lines = file.readlines()
+            this_file = open(fileName)
+            lines = this_file.readlines()
             lines = [i for i in lines if not i.startswith('#')]
-            tuples = [i.strip().split('\t') for i  in lines]
+            tuples = [i.strip().split('\t') for i in lines]
             times = [int(i[0]) for i in tuples]
             temps = [float(i[1]) for i in tuples]
-            if len(temps)>0:
-                temp = sum(temps)/len(temps)
-                temp2 = sum([i*i for i in temps])/len(temps)
+            if len(temps) > 0:
+                temp = sum(temps) / len(temps)
+                temp2 = sum([i * i for i in temps]) / len(temps)
             else:
                 temp = 0
                 temp2 = 0
@@ -45,31 +48,31 @@ class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
                 tempMax = 0
                 timeMin = 0
                 timeMax = 0
-#
-#             # get RMS Temp
-            tempError = math.sqrt(temp2-temp*temp)
-#             ROOT.TMath.RMS(tuple.GetSelectedRows(),tuple.GetV1())
-#
-            if len(temps)>0:
-#             # get Min Temp
+            #
+            # # get RMS Temp
+            tempError = math.sqrt(temp2 - temp * temp)
+            # ROOT.TMath.RMS(tuple.GetSelectedRows(),tuple.GetV1())
+            #
+            if len(temps) > 0:
+                # # get Min Temp
                 tempMin = min(temps)
-    #             #get Max Temp
+                # #get Max Temp
                 tempMax = max(temps)
-                #calculate time difference
+                # calculate time difference
                 timeMin = min(times)
                 timeMax = max(times)
-#
+            #
             duration = timeMax - timeMin
-            temp_List =   array.array('d',temps)
-            time_List = array.array('d',times)
+            temp_List = array.array('d', temps)
+            time_List = array.array('d', times)
             if not self.ResultData['Plot'].has_key('ROOTObjects'):
-                self.ResultData['Plot']['ROOTObjects']={}
-            name = '%02d_%s'%(len(self.ResultData['Plot']['ROOTObjects']),name)
+                self.ResultData['Plot']['ROOTObjects'] = {}
+            name = '%02d_%s' % (len(self.ResultData['Plot']['ROOTObjects']), name)
             if len(temps):
-                graph = ROOT.TGraph(len(temp_List), time_List,temp_List)
+                graph = ROOT.TGraph(len(temp_List), time_List, temp_List)
                 self.ResultData['Plot']['ROOTObjects'][name] = ROOT.TMultiGraph()
             else:
-                graph =  ROOT.TGraph()
+                graph = ROOT.TGraph()
                 self.ResultData['Plot']['ROOTObjects'][name] = ROOT.TGraph()
 
             canvas = self.TestResultEnvironmentObject.Canvas
@@ -89,76 +92,75 @@ class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
             graph.GetYaxis().SetTitleOffset(1.5)
             graph.GetYaxis().CenterTitle()
             graph.Draw("APL")
-            setPoint = 17
+            setPoint = \
+                self.ParentObject.ResultData['SubTestResults']['Summary2'].ResultData['KeyValueDictPairs']['TempC'][
+                    'Value']
             if len(temps):
                 avrgGraph = ROOT.TGraphErrors(2)
                 avrgGraph.SetTitle('')
                 avrgGraph.SetLineColor(ROOT.kRed)
                 avrgGraph.SetLineWidth(2)
-                avrgGraph.SetPoint(0,timeMin,temp)
-                avrgGraph.SetPoint(1,timeMax,temp)
-                avrgGraph.SetPointError(0,0,tempError)
-                avrgGraph.SetPointError(1,0,tempError)
+                avrgGraph.SetPoint(0, timeMin, temp)
+                avrgGraph.SetPoint(1, timeMax, temp)
+                avrgGraph.SetPointError(0, 0, tempError)
+                avrgGraph.SetPointError(1, 0, tempError)
                 avrgGraph.SetFillColor(ROOT.kRed)
                 avrgGraph.SetFillStyle(0)
                 setPointGraph = ROOT.TGraphErrors(2)
                 setPointGraph.SetTitle('')
                 setPointGraph.SetLineColor(ROOT.kBlack)
                 setPointGraph.SetLineWidth(2)
-                setPointGraph.SetPoint(0,timeMin,setPoint)
-                setPointGraph.SetPoint(1,timeMax,setPoint)
-                setPointGraph.SetPointError(0,0,.5)
-                setPointGraph.SetPointError(1,0,.5)
+                setPointGraph.SetPoint(0, timeMin, setPoint)
+                setPointGraph.SetPoint(1, timeMax, setPoint)
+                setPointGraph.SetPointError(0, 0, .5)
+                setPointGraph.SetPointError(1, 0, .5)
                 setPointGraph.SetFillColor(ROOT.kGreen)
                 setPointGraph.SetFillStyle(0)
-                self.ResultData['Plot']['ROOTObjects'][name].Add(setPointGraph,'3L')
-                self.ResultData['Plot']['ROOTObjects'][name].Add(avrgGraph,'3L')
-                self.ResultData['Plot']['ROOTObjects'][name].Add(graph,"L")
+                self.ResultData['Plot']['ROOTObjects'][name].Add(setPointGraph, '3L')
+                self.ResultData['Plot']['ROOTObjects'][name].Add(avrgGraph, '3L')
+                self.ResultData['Plot']['ROOTObjects'][name].Add(graph, "L")
                 self.ResultData['Plot']['ROOTObjects'][name].Draw("a")
                 self.ResultData['Plot']['ROOTObjects'][name].SetTitle(';Time; Temp [#circ C]')
                 self.ResultData['Plot']['ROOTObjects'][name].GetXaxis().SetTimeDisplay(1)
-                self.ResultData['Plot']['ROOTObjects'][name].GetYaxis().SetDecimals();
-                self.ResultData['Plot']['ROOTObjects'][name].GetYaxis().SetTitleOffset(1.5);
-                self.ResultData['Plot']['ROOTObjects'][name].GetYaxis().CenterTitle();
-
+                self.ResultData['Plot']['ROOTObjects'][name].GetYaxis().SetDecimals()
+                self.ResultData['Plot']['ROOTObjects'][name].GetYaxis().SetTitleOffset(1.5)
+                self.ResultData['Plot']['ROOTObjects'][name].GetYaxis().CenterTitle()
 
             self.ResultData['Plot']['ObjectCanvas'][name] = canvas
 
-
-#             tuple.Draw("time:temp","","APL")
-
-
-            #get
-        print 'Analysed "%s"'%fileName
-        print 'Temp: %2.2f +/- %2.2f °C, Min: %2.2f, Max %2.2f'%(temp,tempError,tempMin,tempMax)
-        print 'duration: %s - %s, %s, %s'%(str(datetime.timedelta(seconds=duration)),timeMax-timeMin,timeMax,timeMin)
-        return duration, temp,tempError, tempMin, tempMax
+            # tuple.Draw("time:temp","","APL")
+            # get
+        print 'Analysed "%s"' % fileName
+        print 'Temp: %2.2f +/- %2.2f °C, Min: %2.2f, Max %2.2f' % (temp, tempError, tempMin, tempMax)
+        print 'duration: %s - %s, %s, %s' % (
+            str(datetime.timedelta(seconds=duration)), timeMax - timeMin, timeMax, timeMin)
+        return duration, temp, tempError, tempMin, tempMax
 
     def createTemperaturePlot(self):
         for i in self.ResultData['Plot']['ObjectCanvas']:
             fName = self.GetPlotFileName().split('.')[0].strip()
-            fName = fName+'_'+i+'.svg'
+            fName = fName + '_' + i + '.svg'
             if self.SavePlotFile:
                 canvas = self.ResultData['Plot']['ObjectCanvas'][i]
                 canvas.SaveAs(fName)
             if 'Execute' in i:
                 self.ResultData['Plot']['ROOTObject'] = self.ResultData['Plot']['ROOTObjects'][i]
 
-#         self.ResultData['Plot']['ROOTObject'].SetTitle('');
-#         self.ResultData['Plot']['ROOTObject'].Draw("APL");
-#         self.ResultData['Plot']['ROOTObject'].SetLineColor(4);
-#         self.ResultData['Plot']['ROOTObject'].SetLineWidth(2);
-#
-#         self.ResultData['Plot']['ROOTObject'].GetXaxis().SetTitle("Time ")
-#         self.ResultData['Plot']['ROOTObject'].GetYaxis().SetTitle("Tempeature [#circ C]")
+                # self.ResultData['Plot']['ROOTObject'].SetTitle('');
+                # self.ResultData['Plot']['ROOTObject'].Draw("APL");
+                # self.ResultData['Plot']['ROOTObject'].SetLineColor(4);
+                # self.ResultData['Plot']['ROOTObject'].SetLineWidth(2);
+                #
+                # self.ResultData['Plot']['ROOTObject'].GetXaxis().SetTitle("Time ")
+                # self.ResultData['Plot']['ROOTObject'].GetYaxis().SetTitle("Tempeature [#circ C]")
         if self.ResultData['Plot']['ROOTObject']:
             self.ResultData['Plot']['ROOTObject'].Draw("APL")
 
     def analyseTempPrepare(self):
         Directory = self.ParentObject.RawTestSessionDataPath
         Directory.rstrip('/')
-        fileName = Directory+'/TempLog_Prepare.log'
-        duration, temp, tempError,min,max = self.analyseTemp(fileName)
+        fileName = Directory + '/TempLog_Prepare.log'
+        duration, temp, tempError, min_temp, max_temp = self.analyseTemp(fileName)
         self.durationPrepare = duration
         self.tempPrepare = temp
         self.tempPrepareError = tempError
@@ -167,8 +169,8 @@ class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
     def analyseTempExecute(self):
         Directory = self.ParentObject.RawTestSessionDataPath
         Directory.rstrip('/')
-        fileName = Directory+'/TempLog_Execute.log'
-        duration, temp, tempError,min,max = self.analyseTemp(fileName)
+        fileName = Directory + '/TempLog_Execute.log'
+        duration, temp, tempError, min_temp, max_temp = self.analyseTemp(fileName)
         self.duration = duration
         self.tempTest = temp
         self.tempError = tempError
@@ -176,22 +178,22 @@ class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
     def analyseTempCleanup(self):
         Directory = self.ParentObject.RawTestSessionDataPath
         Directory.rstrip('/')
-        fileName = Directory+'/TempLog_Cleanup.log'
-        duration, temp, tempError,min,max = self.analyseTemp(fileName)
+        fileName = Directory + '/TempLog_Cleanup.log'
+        duration, temp, tempError, min_temp, max_temp = self.analyseTemp(fileName)
         self.durationCleanup = duration
         self.tempCleanup = temp
         self.tempCleanupError = tempError
 
     def PopulateResultData(self):
-#         self.tempPrepare = 0
-#         self.tempPrepareError = 0
-#         self.tempTest = 0
-#         self.tempTestError = 0
-#         self.tempCleanup = 0
-#         self.tempCleanupError = 0
-#         self.duration = 0
-#         self.durationPrepare = 0
-#         self.durationCleanup = 0
+        # self.tempPrepare = 0
+        # self.tempPrepareError = 0
+        # self.tempTest = 0
+        # self.tempTestError = 0
+        # self.tempCleanup = 0
+        # self.tempCleanupError = 0
+        # self.duration = 0
+        # self.durationPrepare = 0
+        #         self.durationCleanup = 0
 
         self.analyseTempPrepare()
         self.analyseTempExecute()
@@ -199,63 +201,63 @@ class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
 
         self.ResultData['KeyValueDictPairs'] = {
             'TemperaturePrepare': {
-                'Value':'%2.2f'%(self.tempPrepare),
-                'Unit':'°C',
+                'Value': '%2.2f' % self.tempPrepare,
+                'Unit': '°C',
                 'Label': 'Temp. preparing test',
             },
             'TemperaturePrepareError': {
-                'Value':'%2.2f'%(self.tempPrepareError),
-                'Unit':'°C',
+                'Value': '%2.2f' % self.tempPrepareError,
+                'Unit': '°C',
                 'Label': 'Temp. error preparing test',
             },
 
             'TemperatureTest': {
-                'Value':'%2.1f'%(self.tempTest),
-                'Unit':'°C',
+                'Value': '%2.1f' % self.tempTest,
+                'Unit': '°C',
                 'Label': 'Temp. while test',
             },
             'TemperatureTestError': {
-                'Value':'%2.2f'%(self.tempError),
-                'Unit':'°C',
+                'Value': '%2.2f' % self.tempError,
+                'Unit': '°C',
                 'Label': 'Temp. error while test',
             },
 
             'TemperatureCleanup': {
-                'Value':'%2.2f'%(self.tempCleanup),
-                'Unit':'°C',
+                'Value': '%2.2f' % self.tempCleanup,
+                'Unit': '°C',
                 'Label': 'Temp. cleaning up test',
             },
             'TemperatureCleanupError': {
-                'Value':'%2.2f'%(self.tempCleanupError),
-                'Unit':'°C',
+                'Value': '%2.2f' % self.tempCleanupError,
+                'Unit': '°C',
                 'Label': 'Temp. error cleaning up test',
             },
 
             'Temperature': {
-                'Value':'%2.2f +/- %2.2f'%(self.tempTest,self.tempError),
-                'Unit':'°C',
+                'Value': '%2.2f +/- %2.2f' % (self.tempTest, self.tempError),
+                'Unit': '°C',
                 'Label': 'Temp. while test',
             },
 
             'Duration': {
-                'Value':'%s'%(str(datetime.timedelta(seconds=self.duration))),
-                'Unit':'',
+                'Value': '%s' % (str(datetime.timedelta(seconds=self.duration))),
+                'Unit': '',
                 'Label': 'Duration of test',
             },
 
             'DurationPrepare': {
-                'Value':'%s'%(str(datetime.timedelta(seconds=self.durationPrepare))),
-                'Unit':'',
+                'Value': '%s' % (str(datetime.timedelta(seconds=self.durationPrepare))),
+                'Unit': '',
                 'Label': 'Duration preparing test',
             },
 
             'DurationCleanup': {
-                'Value':'%s'%(str(datetime.timedelta(seconds=self.duration))),
-                'Unit':'',
+                'Value': '%s' % (str(datetime.timedelta(seconds=self.duration))),
+                'Unit': '',
                 'Label': 'Duration to cleanup test',
             },
         }
-        self.ResultData['KeyList'] = ['Temperature','Duration']
+        self.ResultData['KeyList'] = ['Temperature', 'Duration']
         self.createTemperaturePlot()
         if self.SavePlotFile:
             self.Canvas.SaveAs(self.GetPlotFileName())
