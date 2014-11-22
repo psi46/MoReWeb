e3a593b1868a85635074aa22cb2efc1ba09c5a3e
Esteban Marín <estebanmarin@gmx.ch>
2014-11-22 16:06:31 +0100

    Update QualificationGroup.py

--- Analyse/TestResultClasses/CMSPixel/QualificationGroup/QualificationGroup.py
+++ Analyse/TestResultClasses/CMSPixel/QualificationGroup/QualificationGroup.py
@@ -53,6 +53,7 @@ class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
                 {
                     'Key': 'bareModuletest',
                     'Module': 'BareModuleTest',
+                    'BasedOnModule':'Fulltest',
                     'InitialAttributes': {
                         'StorageKey': 'BareModule_p17_1',
                         'TestResultSubDirectory': '.',
