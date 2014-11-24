60fc599833ad5de953487f4c1cf76e2a47e2adc4
Felix Bachmair <bachmair@phys.ethz.ch>
2014-11-24 14:59:00 +0100

    intention, removed some testoutput

--- Analyse/TestResultClasses/CMSPixel/QualificationGroup/Fulltest/Chips/Chip/TrimBits/TrimBits.py
+++ Analyse/TestResultClasses/CMSPixel/QualificationGroup/Fulltest/Chips/Chip/TrimBits/TrimBits.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
-import ROOT
-import AbstractClasses.Helper.HistoGetter as HistoGetter
-from AbstractClasses.GeneralTestResult import GeneralTestResult
 import os
 import glob
+
+import ROOT
+
+import AbstractClasses.Helper.HistoGetter as HistoGetter
+from AbstractClasses.GeneralTestResult import GeneralTestResult
+
 
 class TestResult(GeneralTestResult):
     def CustomInit(self):
@@ -12,27 +15,27 @@ class TestResult(GeneralTestResult):
         self.Attributes['TestedObjectType'] = 'CMSPixel_QualificationGroup_Fulltest_ROC'
         Directory = self.RawTestSessionDataPath
         FileNamePattern = '/[T,t]rim[p,P]arameters*_C{ChipNo}.dat'.format(ChipNo=self.ParentObject.Attributes['ChipNo'])
-        files = glob.glob('{Directory}/{FileNamePattern}'.format(Directory=Directory,FileNamePattern=FileNamePattern))
+        files = glob.glob('{Directory}/{FileNamePattern}'.format(Directory=Directory, FileNamePattern=FileNamePattern))
         for file in files:
             # print file
             f = file.split('/')[-1].split('.')[0].lower()
             # print f
-            f = f.replace('trimparameters','')
+            f = f.replace('trimparameters', '')
             # print f
             f = f.split('_')
             # print f
             if os.path.isfile(file):
                 TrimParametersFile = open(file, "r")
                 self.ResultData['SubTestResultDictList'] += [
-        	        	        		{
-        	        	        			'Key':'TrimBitParameters'+str(f[0]),
-        	        	        			'Module': 'TrimBitParameters',
-        	        	        			'InitialAttributes': {
-        	        	        				'TrimParametersFile':TrimParametersFile,
-        	        	        				'TrimValue':str(f[0])
-        	        	        			},
-        	        	        		},
-        	        	        	]
+                    {
+                    'Key': 'TrimBitParameters' + str(f[0]),
+                    'Module': 'TrimBitParameters',
+                    'InitialAttributes': {
+                    'TrimParametersFile': TrimParametersFile,
+                    'TrimValue': str(f[0])
+                    },
+                    },
+                ]
 
 
     def PopulateResultData(self):
@@ -42,16 +45,10 @@ class TestResult(GeneralTestResult):
 
         ChipNo = self.ParentObject.Attributes['ChipNo']
         HistoDict = self.ParentObject.ParentObject.ParentObject.HistoDict
-        print 'BLA'
-
-
-        if HistoDict.has_option(self.NameSingle, 'TrimBits'):
-            print 'A'
             histname = HistoDict.get(self.NameSingle, 'TrimBits')
             root_object = HistoGetter.get_histo(self.ParentObject.ParentObject.FileHandle, histname, rocNo=ChipNo)
             # self.ResultData['Plot']['ROOTObject'] = root_object.Clone(self.GetUniqueID())
         else:
-            print 'B'
             histname = HistoDict.get(self.NameSingle, 'TrimBitMap')
             root_object2 = HistoGetter.get_histo(self.ParentObject.ParentObject.FileHandle, histname, rocNo=ChipNo)
             root_object = ROOT.TH1F(self.GetUniqueID(), 'TrimBitDistribution', 17, -.5, 16.5)
@@ -61,16 +58,16 @@ class TestResult(GeneralTestResult):
                     root_object.Fill(entry)
 
         self.ResultData['Plot']['ROOTObject'] = ROOT.TH1F(self.GetUniqueID(), 'TrimBitDistribution', 17, -.5, 16.5)
-        for bin in range(0,root_object.GetNbinsX() + 1):
+        for bin in range(0, root_object.GetNbinsX() + 1):
             bin_content = root_object.GetBinContent(bin)
             bin_center = int(root_object.GetXaxis().GetBinCenter(bin))
-            self.ResultData['Plot']['ROOTObject'].Fill(bin_center,bin_content)
+            self.ResultData['Plot']['ROOTObject'].Fill(bin_center, bin_content)
         mean = 0
         rms = 0
 
         if self.ResultData['Plot']['ROOTObject']:
             # for i in range(self.nCols): # Columns
-            #    for j in range(self.nRows): # Rows
+            # for j in range(self.nRows): # Rows
             #        self.ResultData['Plot']['ROOTObject'].SetBinContent(i+1, j+1, self.ResultData['Plot']['ROOTObject_TrimMap'].GetBinContent(i+1, j+1))
 
             self.ResultData['Plot']['ROOTObject'].SetTitle("")
