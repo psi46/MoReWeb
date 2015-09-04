2f70671ba450b7d32815f71743bdd0d519456aa5
vtavolar <vittorio.tavolaro@cern.ch>
2015-09-04 14:54:49 +0200

    print warning if no readbackCal file is found

--- Analyse/TestResultClasses/CMSPixel/QualificationGroup/Fulltest/Chips/Chip/ReadbackCal/ReadbackCal.py
+++ Analyse/TestResultClasses/CMSPixel/QualificationGroup/Fulltest/Chips/Chip/ReadbackCal/ReadbackCal.py
@@ -4,6 +4,7 @@ import AbstractClasses
 import os
 import re
 from operator import itemgetter
+import warnings
 
 class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
 
@@ -16,83 +17,35 @@ class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
         self.Attributes['TestedObjectType'] = 'CMSPixel_QualificationGroup_Fulltest_ROC'
 
 
-#            self.ResultData['KeyValueDictPairs']['TrimValue'] = {
-#                'Value': '{0:}'.format(trim),
-#                'Label': 'TrimValue'
-#            }
-#            self.ResultData['KeyList'] += ['TrimValue']
-#        for Line in ReadbackFile:
-#                LineArray = Line.strip().split()
-#                RbCalParameterName = LineArray[0]
-#
-#                RbCalParameterValue = float(LineArray[1])
-#                self.ResultData['KeyValueDictPairs'][RbCalParameterName.lower()] = {
-#                    'Value': '{0:.2e}'.format(RbCalParameterValue),
-#                    'Label': RbCalParameterName
-#                }
-#                self.ResultData['KeyList'] += [RbCalParameterName.lower()]
-#            key = 'TrimBitParameters' + self.Attributes['DacParameterTrimValue']
-#            object = \
-#                self.ParentObject.ParentObject.ResultData['SubTestResults']['TrimBits'].ResultData['SubTestResults'][
-#                    key]
-#            self.ResultData['KeyValueDictPairs']['TrimBits_mu'] = object.ResultData['KeyValueDictPairs']['mu']
-#            self.ResultData['KeyValueDictPairs']['TrimBits_mu']['Label'] = 'TrimBit Mean'
-#            self.ResultData['KeyValueDictPairs']['TrimBits_sigma'] = object.ResultData['KeyValueDictPairs']['sigma']
-#            self.ResultData['KeyValueDictPairs']['TrimBits_sigma']['Label'] = 'TrimBit sigma'
-#            self.ResultData['KeyList'] += ['TrimBits_mu', 'TrimBits_sigma']
-
-
     def PopulateResultData(self):
         Directory = self.RawTestSessionDataPath
-        # get all dacParameters files
-#        FileNamePattern = '/readbackCal*_C{ChipNo}.dat'.format(ChipNo=self.ParentObject.Attributes['ChipNo'])
-#        files = glob.glob('{Directory}/{FileNamePattern}'.format(Directory=Directory, FileNamePattern=FileNamePattern))
-#        for file in files:
             
         rbfilename = '{Directory}/readbackCal*_C{ChipNo}.dat'.format(Directory=Directory,ChipNo=self.ParentObject.Attributes['ChipNo'])
         names = glob.glob(rbfilename)
-        # sort by creation date
-        names.sort(key=lambda x: os.stat(os.path.join('', x)).st_mtime)
-        names = [(os.stat(os.path.join('', x)).st_mtime, x) for x in names]
-        names.sort(key = itemgetter(0))
-        # get newest file
-        name = names[-1][1]
-        ReadbackCalFileName = name
-#        name = name.split('/')[-1]
-#
-#        vcalTrim = map(int, re.findall(r'\d+', name))
-#        if len(vcalTrim )==2:
-#            vcalTrim=vcalTrim[0]
-#            i = str(vcalTrim)
-#        else:
-#            vcalTrim =-1
-#            i = ''
+        if(len(names)>0):
+            # sort by creation date
+            names.sort(key=lambda x: os.stat(os.path.join('', x)).st_mtime)
+            names = [(os.stat(os.path.join('', x)).st_mtime, x) for x in names]
+            names.sort(key = itemgetter(0))
+            # get newest file
+            name = names[-1][1]
+            ReadbackCalFileName = name
 
-        if os.path.exists(ReadbackCalFileName):
-            ReadbackCalFile = open(ReadbackCalFileName, "r")
+            if os.path.exists(ReadbackCalFileName):
+                ReadbackCalFile = open(ReadbackCalFileName, "r")
            # self.ResultData['HiddenData']['ReadbcakCal']['File'+i] = ReadbackCalFile
 
-            if ReadbackCalFile :
-                for Line in ReadbackCalFile:
-                    LineArray = Line.strip().split()
-                    RbCalParameterName = LineArray[0]
-
-                    RbCalParameterValue = float(LineArray[1])
-                    self.ResultData['KeyValueDictPairs'][RbCalParameterName.lower()] = {
-                        'Value': '{0:.2e}'.format(RbCalParameterValue),
-                        'Label': RbCalParameterName
+                if ReadbackCalFile :
+                    for Line in ReadbackCalFile:
+                        LineArray = Line.strip().split()
+                        RbCalParameterName = LineArray[0]
+                        
+                        RbCalParameterValue = float(LineArray[1])
+                        self.ResultData['KeyValueDictPairs'][RbCalParameterName.lower()] = {
+                            'Value': '{0:.2e}'.format(RbCalParameterValue),
+                            'Label': RbCalParameterName
                         }
-                    self.ResultData['KeyList'] += [RbCalParameterName.lower()]
-# results  = line.strip().split()
-# if len(results) == 2:
-#     Key, ParameterValue = results
-# else:
-#     a, Key, ParameterValue = results
-#
-# if Key.lower() in ['viref_adc','ibias_dac']:
-#     Key = 'PHScale'
-# if Key.lower() in ['voffsetro','voffsetr0']:
-#     Key = 'PHOffset'
-# Key = Key.lower()
-# self.ResultData['HiddenData']['DacParameters'][Key] = ParameterValue
-                ReadbackCalFile.close()
+                        self.ResultData['KeyList'] += [RbCalParameterName.lower()]
+                    ReadbackCalFile.close()
+        else:
+            warnings.warn('No readback calibration file found!')
