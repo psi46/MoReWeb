14acf0cedd2d7372ca9f68d2397b74cb821e289f
piberger <pberger@phys.ethz.ch>
2015-10-02 15:53:43 +0200

    add fit revision and fit complete flag. do automatic re-fitting if fit-revision has changed and/or fit was aborted (eg. ctrl+c during fit)

--- Analyse/TestResultClasses/CMSPixel/QualificationGroup/Fulltest/Fitting/Fitting.py
+++ Analyse/TestResultClasses/CMSPixel/QualificationGroup/Fulltest/Fitting/Fitting.py
@@ -3,6 +3,7 @@ import AbstractClasses
 from SCurve_Fitting import *
 from PH_Fitting import *
 import ConfigParser
+from AbstractClasses.Helper.BetterConfigParser import BetterConfigParser
 
 class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
 
@@ -12,6 +13,9 @@ class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
         self.Attributes['TestedObjectType'] = 'CMSPixel_Module'
         self.Title = '_'
 
+        self.FitRevisionScurves = 0
+        self.FitRevisionPHlin = 0
+        self.FitRevisionPHtan = 0
 
     def PopulateResultData(self):
 
@@ -37,14 +41,67 @@ class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
         nRocs = self.Attributes['NumberOfChips']
         directory = self.RawTestSessionDataPath
         refit = self.TestResultEnvironmentObject.Configuration['Fitting']['refit']
+
+        refitScurves = refit
+        refitPHlin = refit
+        refitPHtan = refit
+
+        # compare current fit revision with saved revision
+        FitRevDict = BetterConfigParser()
+        fitRevFileName = directory + "/fits.rev"
+        if os.path.isfile(fitRevFileName):
+
+            try:
+                FitRevDict.read(fitRevFileName)
+
+                # if fit procedure changed -> refit
+                if self.FitRevisionScurves > int(FitRevDict.get('FitRevisions', 'SCurves')):
+                    print "info: fit procedure has changed, re-fitting scurves..."
+                    refitScurves = True
+                if self.FitRevisionPHlin > int(FitRevDict.get('FitRevisions', 'PHlin')):
+                    print "info: fit procedure has changed, re-fitting ph lin..."
+                    refitPHlin = True
+                if self.FitRevisionPHtan > int(FitRevDict.get('FitRevisions', 'PHtan')):
+                    print "info: fit procedure has changed, re-fitting ph tan..."
+                    refitPHtan = True
+
+                # if last fitting was not done completely (eg. STRG+C while fit...) -> refit
+                if FitRevDict.get('Fit', 'Complete').strip().lower() != 'true':
+                    print "info: last fit was incomplete! re-fitting all..."
+                    refitScurves = True
+                    refitPHlin = True
+                    refitPHtan = True
+            except:
+                print "warning: could not read fit revision file!"
+
+        else:
+            FitRevDict.add_section('FitRevisions')
+            FitRevDict.add_section('Fit')
+
+        # unset complete flag in file
+        FitRevDict.set('Fit','Complete', 'false')
+        with open(fitRevFileName, 'wb') as fitRevFile:
+            FitRevDict.write(fitRevFile)
+
         print 'SCurve fitting...'
         ePerVcal =  self.TestResultEnvironmentObject.GradingParameters['StandardVcal2ElectronConversionFactor']
-        fitter = SCurve_Fitting(refit,HistoDict = self.ParentObject.HistoDict,ePerVcal=ePerVcal, ParallelProcessing=ParallelProcessing, LimitProcesses=LimitProcesses)
+        fitter = SCurve_Fitting(refit=refitScurves, HistoDict=self.ParentObject.HistoDict,ePerVcal=ePerVcal, ParallelProcessing=ParallelProcessing, LimitProcesses=LimitProcesses)
         fitter.FitAllSCurve(directory,nRocs)
         print 'linear PH fitting...'
-        fitter = PH_Fitting(0,refit,HistoDict = self.ParentObject.HistoDict, ParallelProcessing=ParallelProcessing, LimitProcesses=LimitProcesses)
+        fitter = PH_Fitting(fitMode=0, refit=refitPHlin, HistoDict=self.ParentObject.HistoDict, ParallelProcessing=ParallelProcessing, LimitProcesses=LimitProcesses)
         fitter.FitAllPHCurves(directory,nRocs)
         print 'tanh PH fitting...'
-        fitter = PH_Fitting(3,refit,HistoDict = self.ParentObject.HistoDict, ParallelProcessing=ParallelProcessing, LimitProcesses=LimitProcesses)
+        fitter = PH_Fitting(fitMode=3, refit=refitPHtan, HistoDict=self.ParentObject.HistoDict, ParallelProcessing=ParallelProcessing, LimitProcesses=LimitProcesses)
         fitter.FitAllPHCurves(directory,nRocs)
+
+        # update saved revisions
+        FitRevDict.set('FitRevisions','SCurves', str(self.FitRevisionScurves))
+        FitRevDict.set('FitRevisions','PHlin', str(self.FitRevisionPHlin))
+        FitRevDict.set('FitRevisions','PHtan', str(self.FitRevisionPHtan))
+
+        # unset complete flag in file
+        FitRevDict.set('Fit','Complete', 'true')
+        with open(fitRevFileName, 'wb') as fitRevFile:
+            FitRevDict.write(fitRevFile)
+
         print 'done'
--- Analyse/TestResultClasses/CMSPixel/QualificationGroup/XRayHRQualification/Fitting/Fitting.py
+++ Analyse/TestResultClasses/CMSPixel/QualificationGroup/XRayHRQualification/Fitting/Fitting.py
@@ -3,12 +3,15 @@ import AbstractClasses
 import ConfigParser
 from AbstractClasses.Helper.BetterConfigParser import BetterConfigParser
 from TestResultClasses.CMSPixel.QualificationGroup.Fulltest.Fitting.SCurve_Fitting import *
+import os
 
 class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
     def CustomInit(self):
         self.Name='CMSPixel_QualificationGroup_XRayHRQualification_Fitting_TestResult'
         self.NameSingle='Fitting'
         self.Attributes['TestedObjectType'] = 'CMSPixel_Module'
+
+        self.FitRevisionScurves = 0
 
     def PopulateResultData(self):
 
@@ -42,5 +45,44 @@ class TestResult(AbstractClasses.GeneralTestResult.GeneralTestResult):
         HistoDict.set('SCurveFitting','inputFileName', self.ParentObject.ParentObject.HistoDict.get('HighRate', 'SCurveDataFileName'))
 
         ePerVcal =  self.TestResultEnvironmentObject.GradingParameters['StandardVcal2ElectronConversionFactor']
-        fitter = SCurve_Fitting(refit,HistoDict = HistoDict,ePerVcal=ePerVcal, ParallelProcessing=ParallelProcessing, LimitProcesses=LimitProcesses)
+
+        # compare current fit revision with saved revision
+        FitRevDict = BetterConfigParser()
+        fitRevFileName = directory + "/fits.rev"
+        if os.path.isfile(fitRevFileName):
+
+            try:
+                FitRevDict.read(fitRevFileName)
+
+                # if fit procedure changed -> refit
+                if self.FitRevisionScurves > int(FitRevDict.get('FitRevisions', 'SCurves')):
+                    print "info: fit procedure has changed, re-fitting..."
+                    refit = True
+
+                # if last fitting was not done completely (eg. STRG+C while fit...) -> refit
+                if FitRevDict.get('Fit', 'Complete').strip().lower() != 'true':
+                    print "info: last fit was incomplete! re-fitting..."
+                    refit = True
+            except:
+                print "warning: could not read fit revision file!"
+
+        else:
+            FitRevDict.add_section('FitRevisions')
+            FitRevDict.add_section('Fit')
+
+        # unset complete flag in file
+        FitRevDict.set('Fit','Complete', 'false')
+        with open(fitRevFileName, 'wb') as fitRevFile:
+            FitRevDict.write(fitRevFile)
+
+        # do fitting
+        fitter = SCurve_Fitting(refit=refit, HistoDict=HistoDict, ePerVcal=ePerVcal, ParallelProcessing=ParallelProcessing, LimitProcesses=LimitProcesses)
         fitter.FitAllSCurve(directory, nRocs)
+
+        # update saved revision
+        FitRevDict.set('FitRevisions','SCurves', str(self.FitRevisionScurves))
+
+        # set complete flag in file
+        FitRevDict.set('Fit','Complete', 'true')
+        with open(fitRevFileName, 'wb') as fitRevFile:
+            FitRevDict.write(fitRevFile)
