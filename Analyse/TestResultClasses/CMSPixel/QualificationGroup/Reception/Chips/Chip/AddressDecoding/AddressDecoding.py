f8740b47ad7058e93675d6ef02f01a18313bd657
piberger <pberger@phys.ethz.ch>
2016-01-26 20:36:51 +0100

    commit missing files

---
+++ Analyse/TestResultClasses/CMSPixel/QualificationGroup/Reception/Chips/Chip/AddressDecoding/AddressDecoding.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+import TestResultClasses.CMSPixel.QualificationGroup.Fulltest.Chips.Chip.AddressDecoding.AddressDecoding
+
+class TestResult(TestResultClasses.CMSPixel.QualificationGroup.Fulltest.Chips.Chip.AddressDecoding.AddressDecoding.TestResult):
+    def CustomInit(self):
+        super(TestResult, self).CustomInit()
