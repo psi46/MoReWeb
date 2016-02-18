e2deb491e597905ef3d25f1cc263a56816ecd64f
piberger <pberger@phys.ethz.ch>
2016-02-18 10:41:27 +0100

    fix iv y-axis labels in reception test

--- Analyse/TestResultClasses/CMSPixel/QualificationGroup/Reception/IVCurve/IVCurve.py
+++ Analyse/TestResultClasses/CMSPixel/QualificationGroup/Reception/IVCurve/IVCurve.py
@@ -7,3 +7,8 @@ import TestResultClasses.CMSPixel.QualificationGroup.Fulltest.IVCurve.IVCurve
 class TestResult(TestResultClasses.CMSPixel.QualificationGroup.Fulltest.IVCurve.IVCurve.TestResult):
     def CustomInit(self):
         super(TestResult, self).CustomInit()
+
+    def PopulateResultData(self):
+        super(TestResult, self).PopulateResultData()
+        self.ResultData['Plot']['ROOTObject'].GetYaxis().SetMoreLogLabels()
+        self.SaveCanvas()
