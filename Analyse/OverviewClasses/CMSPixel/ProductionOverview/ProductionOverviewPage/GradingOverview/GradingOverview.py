44d2ee823be594532a4ed6294fa490356ed3ca22
piberger <pberger@phys.ethz.ch>
2016-03-29 11:35:17 +0200

    show required tests for completeness in grading overview box and count number of modules degraded in FQ

--- Analyse/OverviewClasses/CMSPixel/ProductionOverview/ProductionOverviewPage/GradingOverview/GradingOverview.py
+++ Analyse/OverviewClasses/CMSPixel/ProductionOverview/ProductionOverviewPage/GradingOverview/GradingOverview.py
@@ -26,6 +26,7 @@ class ProductionOverview(AbstractClasses.GeneralProductionOverview.GeneralProduc
         FT17_Grades = []
         XrayCal_Grades = []
         XrayHR_Grades = []
+        FullQualification_Grades = []
         Final_Grades = []
 
         for ModuleID in ModuleIDsList:
@@ -66,12 +67,20 @@ class ProductionOverview(AbstractClasses.GeneralProductionOverview.GeneralProduc
             FTMinus20BTC_Grades.append(FTMinus20BTC)
             FTMinus20ATC_Grades.append(FTMinus20ATC)
             FT17_Grades.append(FT17)
+
+            if 'C' in [FTMinus20BTC, FTMinus20ATC, FT17]:
+                FullQualification_Grades.append('C')
+            elif 'B' in [FTMinus20BTC, FTMinus20ATC, FT17]:
+                FullQualification_Grades.append('B')
+            elif 'A' in [FTMinus20BTC, FTMinus20ATC, FT17]:
+                FullQualification_Grades.append('A')
+
             XrayCal_Grades.append(XrayCal)
             XrayHR_Grades.append(XrayHR)
 
         TableData = [
             [{'Class' : 'Header', 'Value' : 'Tested modules:'}, {'Class' : 'Value', 'Value' : "%d"%len(ModuleIDsList)}],
-            [{'Class' : 'Header', 'Value' : 'Qualification complete:'}, {'Class' : 'Value', 'Value' : "%d"%len(Final_Grades)}],
+            [{'Class' : 'Header', 'Value' : 'Qualification complete*:'}, {'Class' : 'Value', 'Value' : "%d"%len(Final_Grades)}],
         ]
         HTML = self.Table(TableData)
 
@@ -93,6 +102,9 @@ class ProductionOverview(AbstractClasses.GeneralProductionOverview.GeneralProduc
             [{'Class' : 'Value', 'Value' : 'T = +17 ATC'}, {'Class' : 'Value', 'Value' : "%d"%len([x for x in FT17_Grades if x=='A']) }, {'Class' : 'Value', 'Value' : "%d"%len([x for x in FT17_Grades if x=='B']) }, {'Class' : 'Value', 'Value' : "%d"%len([x for x in FT17_Grades if x=='C']) }]
         )
 
+        TableData.append(
+            [{'Class' : 'Value', 'Value' : 'FullQualification'}, {'Class' : 'Value', 'Value' : "%d"%len([x for x in FullQualification_Grades if x=='A']) }, {'Class' : 'Value', 'Value' : "%d"%len([x for x in FullQualification_Grades if x=='B']) }, {'Class' : 'Value', 'Value' : "%d"%len([x for x in FullQualification_Grades if x=='C']) }]
+        )
         ### X-ray
         TableData.append(    
             [{'Class' : 'Value', 'Value' : 'High Rate'}, {'Class' : 'Value', 'Value' : "%d"%len([x for x in XrayHR_Grades if x=='A']) }, {'Class' : 'Value', 'Value' : "%d"%len([x for x in XrayHR_Grades if x=='B']) }, {'Class' : 'Value', 'Value' : "%d"%len([x for x in XrayHR_Grades if x=='C']) }]
@@ -116,7 +128,12 @@ class ProductionOverview(AbstractClasses.GeneralProductionOverview.GeneralProduc
         except:
             VersionInfo = VersionInfo + "unknown version"
 
-        HTML += self.Table(TableData) + self.BoxFooter("<div style='height:10px;'></div><div style='text-align:center;' title='fraction of A+B modules'><b>Yield: %s</b></div><div style='height:10px;'></div><div>%s</div>"%(TotalYield, VersionInfo))
+        try:
+            Footnotes = '*: required: ' + ','.join(self.TestResultEnvironmentObject.Configuration['RequiredTestTypesForComplete'].strip().split(','))
+        except:
+            Footnotes = ''
+
+        HTML += self.Table(TableData) + self.BoxFooter("<div style='height:10px;'></div><div style='text-align:center;' title='fraction of A+B modules'><b>A+B: %d modules, yield: %s</b></div><div style='height:10px;'></div><div>%s</div><div>%s</div>"%(nFinalA + nFinalB, TotalYield, VersionInfo, Footnotes))
 
         return self.Boxed(HTML)
 
