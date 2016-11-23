33a8510b753b596d66a38e9d6cf4db19e37e07ed
piberger <pberger@phys.ethz.ch>
2016-11-23 11:52:31 +0100

    adjust y scale for pedestal spread for L1 modules

--- Analyse/TestResultClasses/CMSPixel/QualificationGroup/Fulltest/PedestalSpread/PedestalSpread.py
+++ Analyse/TestResultClasses/CMSPixel/QualificationGroup/Fulltest/PedestalSpread/PedestalSpread.py
@@ -12,14 +12,18 @@ class TestResult(GeneralTestResult):
             'DataKey': 'PHCalibrationPedestal',  # which sub test result to take the data from
             'DataParameterKey': 'sigma',  # which part of key value dict pairs
             'DataFactor': self.TestResultEnvironmentObject.GradingParameters['StandardVcal2ElectronConversionFactor'],
-            'YLimitB': self.TestResultEnvironmentObject.GradingParameters['pedestalB'],  # limit for grading
-            'YLimitC': self.TestResultEnvironmentObject.GradingParameters['pedestalC'],  # limit for grading
+            'YLimitB': self.TestResultEnvironmentObject.GradingParameters['pedestalB'] if not self.isPROC else 1e9,  # limit for grading
+            'YLimitC': self.TestResultEnvironmentObject.GradingParameters['pedestalC'] if not self.isPROC else 1e9,  # limit for grading
             'MarkerColor': ROOT.kRed,
             'LineColor': ROOT.kRed,
             'MarkerStyle': 21,
             'YaxisTitle': 'Average Pedestal [e]',
-
         }
+
+        if self.isPROC:
+            self.Attributes['SpecialPopulateDataParameters']['Ymin'] = 0
+            self.Attributes['SpecialPopulateDataParameters']['Ymax'] = 10000
+
     def PopulateResultData(self):
         ROOT.gPad.SetLogy(0)
 
