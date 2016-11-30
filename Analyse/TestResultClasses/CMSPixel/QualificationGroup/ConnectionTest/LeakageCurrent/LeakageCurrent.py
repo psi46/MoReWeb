393687ab15b91d26fc4b2e71745c51c85d230761
piberger <pberger@phys.ethz.ch>
2016-11-30 16:45:35 +0100

    commit missing leakage current test files'

---
+++ Analyse/TestResultClasses/CMSPixel/QualificationGroup/ConnectionTest/LeakageCurrent/LeakageCurrent.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import AbstractClasses
+import TestResultClasses
+import TestResultClasses.CMSPixel.QualificationGroup.OnShellQuickTest.LeakageCurrent.LeakageCurrent
+class TestResult(TestResultClasses.CMSPixel.QualificationGroup.OnShellQuickTest.LeakageCurrent.LeakageCurrent.TestResult):
+    def CustomInit(self):
+        # Call Overridden CustomInit()
+        super(TestResult, self).CustomInit()
